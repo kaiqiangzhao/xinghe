8bb94e79e6b652b041350cd5d56177d6fa58787b
zhaokaiqiang <kaiqiang.zh@gmail.com>
2023-05-19 23:32:44 +0800

    feat: p 2 span

--- pages/index.tsx
+++ pages/index.tsx
@@ -155,9 +155,9 @@ const Home: NextPage = () => {
                   Your generated bios
                 </h2>
               </div>
-              <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
+              <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto  whitespace-pre-wrap">
                 <div
-                  className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                  className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border text-left"
                   onClick={() => {
                     navigator.clipboard.writeText(generatedBios);
                     toast("Bio copied to clipboard", {
