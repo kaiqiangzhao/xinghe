fab1b544ead26bc0a7672955b376ec464bfd9485
zhaokaiqiang <kaiqiang.zh@gmail.com>
2023-05-20 11:54:56 +0800

    feat: p 2 span

--- pages/index.tsx
+++ pages/index.tsx
@@ -83,7 +83,6 @@ const Home: NextPage = () => {
               src="/1-black.png"
               width={30}
               height={30}
-              // alt="1 icon"
               className="mb-5 sm:mb-0"
             />
             <p className="text-left font-medium">
