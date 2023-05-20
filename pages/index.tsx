4324fae06931d54a9c49a0d07334dd346447a6c5
zhaokaiqiang <kaiqiang.zh@gmail.com>
2023-05-20 11:24:49 +0800

    feat: p 2 span

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,38 +68,28 @@ const Home: NextPage = () => {
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Twitter Bio Generator</title>
+        <title>Creatip</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
-        <a
-          className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
-          href="https://github.com/Nutlope/twitterbio"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Github />
-          <p>Star on GitHub</p>
-        </a>
         <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
-          Generate your next Twitter bio using chatGPT
+          使用 ChatGPT 获取创作灵感
         </h1>
-        <p className="text-slate-500 mt-5">47,118 bios generated so far.</p>
         <div className="max-w-xl w-full">
           <div className="flex mt-10 items-center space-x-3">
             <Image
               src="/1-black.png"
               width={30}
               height={30}
-              alt="1 icon"
+              // alt="1 icon"
               className="mb-5 sm:mb-0"
             />
             <p className="text-left font-medium">
-              Copy your current bio{" "}
+              输入事件{" "}
               <span className="text-slate-500">
-                (or write a few sentences about yourself)
+                (基于此事件生成创作灵感)
               </span>
               .
             </p>
@@ -110,23 +100,23 @@ const Home: NextPage = () => {
             rows={4}
             className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
             placeholder={
-              "e.g. Senior Developer Advocate @vercel. Tweeting about web development, AI, and React / Next.js. Writing nutlope.substack.com."
+              "如：脱口秀演员因不当言论被热议"
             }
           />
-          <div className="flex mb-5 items-center space-x-3">
-            <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
-            <p className="text-left font-medium">Select your vibe.</p>
-          </div>
-          <div className="block">
-            <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
-          </div>
+          {/*<div className="flex mb-5 items-center space-x-3">*/}
+          {/*  <Image src="/2-black.png" width={30} height={30} alt="1 icon" />*/}
+          {/*  <p className="text-left font-medium">Select your vibe.</p>*/}
+          {/*</div>*/}
+          {/*<div className="block">*/}
+          {/*  <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />*/}
+          {/*</div>*/}
 
           {!loading && (
             <button
               className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
               onClick={(e) => generateBio(e)}
             >
-              Generate your bio &rarr;
+              获取创作灵感 &rarr;
             </button>
           )}
           {loading && (
@@ -152,7 +142,7 @@ const Home: NextPage = () => {
                   className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
                   ref={bioRef}
                 >
-                  Your generated bios
+                  创作灵感
                 </h2>
               </div>
               <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto  whitespace-pre-wrap">
