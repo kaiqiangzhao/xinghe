c4411bf81799c83000f025e4969a076164bb741c
zhaokaiqiang <kaiqiang.zh@gmail.com>
2023-05-20 21:56:40 +0800

    feat: p 2 span

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
         <Image
           alt="Vercel Icon"
           src="/xinghelogo.png"
-          className="sm:w-8 sm:h-[27px] w-8 h-[28px]"
+          className="sm:w-8 sm:h-[32px] w-8 h-[32px]"
           width={32}
           height={32}
         />
